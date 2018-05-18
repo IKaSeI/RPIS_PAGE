ffb492457eee77bc58eb18ca8449368476156434
IKaSeI <36835524+IKaSeI@users.noreply.github.com>
2018-05-18 17:09:40 +0300

    There are all Models, Views and Controller

---
+++ Models/BookMarks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMGs.Models
+{
+    public class BookMarks
+    {
+        public int Id { get; set; }
+        public int UserId{get;set;}
+        public int OrderId { get; set; }
+
+
+
+
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMGs.Models
+{
+    public enum OrderType { Sale, Purchase, Lease, Rent, Swap }
+    public class Order
+    {
+        public int Id { get; set; }//number of order
+
+        public String Priece { get; set; }
+
+        public OrderType Type { get; set; }
+
+        public int UserId { get; set; }//link to user
+
+        public String UserName { get; set; }
+
+        public int RealEstateId { get; set; }//link to real estate
+        public RealEstate RealEstate { get; set; }//
+    }
+}
